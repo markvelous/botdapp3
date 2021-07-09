8d042d81c38f9d8a33cd1832a201fd21f1bf4668
markvelous <85272691+markvelous@users.noreply.github.com>
2021-07-09 11:37:55 +0800

    debug

--- src/components/App.js
+++ src/components/App.js
@@ -120,7 +120,7 @@ class App extends Component {
             { this.state.bots.map((bot, key) => {
               return(
                 <div key={key} className="col-md-2 mb-3">
-                  <div className="botname">{bot}</div>
+                  <div className="botname">- {bot} -</div>
                   <div className="token"><img src={'https://robohash.org/' + bot + '?size=150x150'} alt="" /></div>
                   <br/><br/><br/>
                 </div>
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
+require('truffle-hdwallet-provider')
 
 module.exports = {
   networks: {
@@ -9,6 +11,21 @@ module.exports = {
       network_id: "*" // Match any network id
     },
   },
+
+  /*
+  const HDWalletProvider = require('truffle-hdwallet-provider')
+
+  rinkeby: {
+    provider: (new HDWalletProvider(PRIVATE_KEY, API_URL)),
+    network_id: 4,
+    gas: 4500000,
+    gasPrice: 10000000000,
+    confirmations: 2,   
+    timeoutBlocks: 200,
+    skipDryRun: true
+  }
+  */
+
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
